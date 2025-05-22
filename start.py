6fabbf155aa52f0303614dbe808035105de72589
Armita <colorfulatoms@gmail.com>
2025-05-22 11:32:18 +0330

    Revert "change variable names"
    This reverts commit 9a0086a74a58171f5726f040b503f589e0651047.

--- start.py
+++ start.py
@@ -1,2 +1,22 @@
-message = "Boo!"
-print(message)
+# print("hello world!") (interesting! :O)
+####### comp.lang.python######
+# Probably your most valuable Python resource, right after python.org.
+# Spot Python luminaries in their native habitat! 
+# Don't be surprised to have your questions answered by the original programmer or the author of the book open on your desk!
+# The best thing about comp.lang.python is how "newbie-friendly" the mail group is.
+# You can ask any question and never get a "RTFM" thrown back at you.
+
+# A = [2, "mammoth", 'A', 8, 10]
+# print("the first element is: ", A[0])
+
+# print("length is: ", len(A))
+
+# print("the element with negative index is: ", A[2:4])
+
+
+# print(*A, sep = ' >:3 ')
+
+str = 'Hello my goofy friend \'maryam\' how do you do?'
+str2 = "Gandhi is a 'great man'! surely!\n do you want to //drink a tee?"
+print(str)
+print(str2)
